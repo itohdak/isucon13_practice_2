250bba83ed1147f0a63c3c3086beee076e28ee7a
itohdak <itohdak@gmail.com>
2024-12-05 02:28:16 +0900

    disable logs

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -15,13 +15,9 @@ import (
 	"github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 
 	"github.com/gorilla/sessions"
 	"github.com/labstack/echo-contrib/session"
-	echolog "github.com/labstack/gommon/log"
-
-	"github.com/kaz/pprotein/integration/standalone"
 )
 
 const (
@@ -127,12 +123,12 @@ func initializeHandler(c echo.Context) error {
 }
 
 func main() {
-	go standalone.Integrate(":8888")
+	// go standalone.Integrate(":8888")
 
 	e := echo.New()
-	e.Debug = true
-	e.Logger.SetLevel(echolog.DEBUG)
-	e.Use(middleware.Logger())
+	// e.Debug = true
+	// e.Logger.SetLevel(echolog.DEBUG)
+	// e.Use(middleware.Logger())
 	cookieStore := sessions.NewCookieStore(secret)
 	cookieStore.Options.Domain = "*.u.isucon.local"
 	e.Use(session.Middleware(cookieStore))
